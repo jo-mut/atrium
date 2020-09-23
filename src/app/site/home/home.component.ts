d76b4dec0822fb82318076cdac76e82a30efd079
jo-mut <johnmutuku628@gmail.com>
2020-09-23 14:34:24 +0300

    added the sites url so it doesnt fail on reload

--- src/app/site/home/home.component.ts
+++ src/app/site/home/home.component.ts
@@ -62,21 +62,21 @@ export class HomeComponent implements OnInit {
     private config: NgbCarouselConfig,
     private matDialog: MatDialog,
     private dbOperations: DbOperationsService) {
-      this.carouselConfig();
-      this.purposeOfCompetion();
-      this.getInspiration();
-   }
+    this.carouselConfig();
+    this.purposeOfCompetion();
+    this.getInspiration();
+  }
 
   ngOnInit(): void {
     this.getSampleArtworks();
     this.projectHost();
-  
+
   }
 
   carouselConfig() {
     this.config.interval = 3000;
     this.config.keyboard = false;
-    this.config.pauseOnHover = true;
+    this.config.pauseOnHover = false;
     this.config.wrap = true;
   }
 
@@ -94,7 +94,7 @@ export class HomeComponent implements OnInit {
   onPlayerReady(api: VgApiService) {
     this.api = api;
     this.api.getDefaultMedia().subscriptions
-    .loadedMetadata.subscribe(this.playVideo.bind(this));
+      .loadedMetadata.subscribe(this.playVideo.bind(this));
     this.api.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
   }
 
@@ -120,7 +120,7 @@ export class HomeComponent implements OnInit {
     //   this.showInspiration = true;
     // }
 
-    return  this.inspiration.substring(0, 600);
+    return this.inspiration.substring(0, 600);
   }
 
 
@@ -149,7 +149,7 @@ export class HomeComponent implements OnInit {
     //   this.showPurpose = true;
     //   this.purpose.substring(0, 600);
     // }
-    
+
     return this.purpose.substring(0, 600);
   }
 
@@ -209,14 +209,14 @@ export class HomeComponent implements OnInit {
 
   }
 
- 
+
 
   projectHost() {
     this.hosts = [
       {
-        id:0,
-        flag:'assets/images/gallery/hosts/bobby.jpg',
-        title:'Project Lead',
+        id: 0,
+        flag: 'assets/images/gallery/hosts/bobby.jpg',
+        title: 'Project Lead',
         name: 'Bobby Pall',
         lessInfo: ` Bobby Pall is a modern day photographic artist who believes that photography is the ideal medium for ...`,
         bio: `
@@ -230,9 +230,9 @@ export class HomeComponent implements OnInit {
         I am the author of Vanishing Songs of the Warriors, a coffee table book that focuses on the resilience, passion and beauty of indigenous communities. As Lead Photographer, I have also partnered with Footprints Press on the following coffee table books: Life Journeys; Scaling Heights, Life Journeys; Seeking Destiny, Wisdom of the Elders, Aspiration of a Generation; Youth of Kenya. These are books featuring role models of Kenya for posterity.`
       },
       {
-        id:1,
-        flag:'assets/images/gallery/hosts/george.jpg',
-        title:'Writer/Documentary Filmmaker',
+        id: 1,
+        flag: 'assets/images/gallery/hosts/george.jpg',
+        title: 'Writer/Documentary Filmmaker',
         name: 'George Muiruri',
         lessInfo: ` I have had innumerable restarts in my life; start and stop periods. Start periods that birthed new life ...`,
         bio: `I have had innumerable restarts in my life; start and stop periods. Start periods that birthed new life; stop periods at junctions seeking direction.  Each, the genesis of a new opportunity, a new perception, a new idea, a new love, a new…, a new… 
@@ -241,9 +241,9 @@ export class HomeComponent implements OnInit {
         `
       },
       {
-        id:2,
-        flag:'assets/images/gallery/hosts/edna.jpg',
-        title:'Finance Specialist/Economist',
+        id: 2,
+        flag: 'assets/images/gallery/hosts/edna.jpg',
+        title: 'Finance Specialist/Economist',
         name: 'Edna Kalumbini, MA',
         lessInfo: `I am Edna Kalumbini and I see myself as a high-impact, solutions-oriented, team player and Finance Specialist ...`,
         bio: `I am Edna Kalumbini and I see myself as a high-impact, solutions-oriented, team player and Finance Specialist. Accounting and management of the project accounts is my forte and I excel in delivering monthly financial reports, accounts reconciliation, planning and financial forecasting. My skills and expertise in bringing order to projects have benefited the organisations I have supported in the private sector and the non-profit sector. 
@@ -252,33 +252,32 @@ export class HomeComponent implements OnInit {
 
       },
       {
-        id:3,
-        flag:'assets/images/gallery/hosts/lucy.jpg',
-        title:'Editor/Writer',
+        id: 3,
+        flag: 'assets/images/gallery/hosts/lucy.jpg',
+        title: 'Editor/Writer',
         name: 'Lucy Mwangi',
-        lessInfo: `I am Edna Kalumbini and I see myself as a high-impact, solutions-oriented, team player and Finance Specialist ...`,
-        bio: `My name is Lucy Mwangi and I am excited to provide a brief insight into who I am and what drives me. I am a storytelling enthusiast that has lent her skills in the end-to-end production of over 20 publications that strive to tell the African story from an African perspective. Over the last two decades, I have partnered with research organisations, profit organisations, publishing houses, and independent writers/authors to provide editorial and communication expertise.
-        It is now a real pleasure to lend my skills and years of experience to ensure the success of ‘African Resilience in the Wake of a Pandemic’, a photography and videography campaign under the Mastercard Foundation COVID-19 Public Awareness Campaign. The Campaign is a great opportunity to witness and celebrate as the Youth of Africa use their creativity to open up about how Covid-19 has affected their lives. It is a privilege to work on this Campaign.
-        `
+        lessInfo: `My name is Lucy Mwangi and I am excited to provide a brief insight into who I am and what drives me. I am a ...`,
+        bio: `   My name is Lucy Mwangi and I am excited to provide a brief insight into who I am and what drives me. I am a storytelling enthusiast that has lent her skills in the end-to-end production of over 20 publications that strive to tell the African story from an African perspective. Over the last two decades, I have partnered with research organisations, profit organisations, publishing houses, and independent writers/authors to provide editorial and communication expertise.
+        It is now a real pleasure to lend my skills and years of experience to ensure the success of ‘African Resilience in the Wake of a Pandemic’, a photography and videography campaign under the Mastercard Foundation COVID-19 Public Awareness Campaign. The Campaign is a great opportunity to witness and celebrate as the Youth of Africa use their creativity to open up about how Covid-19 has affected their lives. It is a privilege to work on this Campaign.`
 
       },
       {
-        id:4,
-        flag:'assets/images/gallery/hosts/christine.jpg',
+        id: 4,
+        flag: 'assets/images/gallery/hosts/christine.jpg',
         name: 'Christine Njoki Kuriah, MPA',
         title: 'Operations and Systems Development',
-        lessInfo: `I am Christine Njoki Kuriah and I am delighted to tell you a little bit about myself. I am a passionate individual ...`,
-        bio: `I am Christine Njoki Kuriah and I am delighted to tell you a little bit about myself. I am a passionate individual who champions causes that promote social justice and equality, civil rights and democracy. For the past two decades, I have partnered with organizations around the globe to bring my operations and systems development/management and resource mobilization expertise to both the non-profit and public service sectors in the Healthcare, Education, Advocacy and Community Development fields.
-        Now, I am excited to use some of my professional skills and experience and be a part of ‘African Resilience in the Wake of a Pandemic’, photography and videography campaign under the MasterCard Foundation Covid-19 Public Awareness Campaign. I get to see and hear, first-hand, the dynamic stories told by the Youth in Africa, through photographs and videos, about their deeply personal experiences as they navigate life through this global pandemic. What better way is there for them to express themselves in an honest and authentic way while letting us into their world as they convey their stories? I am honoured to be a part of this amazing Campaign.
+        lessInfo: `I am Christine Njoki Kuriah and I am delighted to tell you a little bit about myself.I am a passionate individual ...`,
+        bio: `I am Christine Njoki Kuriah and I am delighted to tell you a little bit about myself.I am a passionate individual who champions causes that promote social justice and equality, civil rights and democracy.For the past two decades, I have partnered with organizations around the globe to bring my operations and systems development/ management and resource mobilization expertise to both the non - profit and public service sectors in the Healthcare, Education, Advocacy and Community Development fields.
+        Now, I am excited to use some of my professional skills and experience and be a part of ‘African Resilience in the Wake of a Pandemic’, photography and videography campaign under the MasterCard Foundation Covid - 19 Public Awareness Campaign.I get to see and hear, first - hand, the dynamic stories told by the Youth in Africa, through photographs and videos, about their deeply personal experiences as they navigate life through this global pandemic.What better way is there for them to express themselves in an honest and authentic way while letting us into their world as they convey their stories ? I am honoured to be a part of this amazing Campaign.
         `
       }, {
-        id:5,
-        flag:'assets/images/gallery/hosts/xotichil.jpg',
+        id: 5,
+        flag: 'assets/images/gallery/hosts/xotichil.jpg',
         name: 'Xochitl Ramirez',
         title: 'Industrial Designer and, Public Policy and Social Project Developer',
-        lessInfo: `My twenty (20) years as a Designer have taught me that being creative and designing out of the box is about the passion ... `,
-        bio: `My twenty (20) years as a Designer have taught me that being creative and designing out of the box is about the passion for creativity that one nurtures in their inner-most being and is not limited to the equipment or software at hand. I am a risk taker who is passionate about empowering all people regardless of race, age, or creeds. This is me, Xochitl Ramirez—creativity and social development are my two great passions. 
-        It is a privilege to be part of this interdisciplinary team that has taken on the task of ensuring the success of ‘African Resilience in the Wake of a Pandemic’, a photography and videography campaign under the Mastercard Foundation COVID-19 Public Awareness Campaign. I am eager to participate in putting the creative storytelling capabilities of young African through the lens. 
+        lessInfo: `My twenty(20) years as a Designer have taught me that being creative and designing out of the box is about the passion ... `,
+        bio: `My twenty(20) years as a Designer have taught me that being creative and designing out of the box is about the passion for creativity that one nurtures in their inner - most being and is not limited to the equipment or software at hand.I am a risk taker who is passionate about empowering all people regardless of race, age, or creeds.This is me, Xochitl Ramirez—creativity and social development are my two great passions.
+    It is a privilege to be part of this interdisciplinary team that has taken on the task of ensuring the success of ‘African Resilience in the Wake of a Pandemic’, a photography and videography campaign under the Mastercard Foundation COVID - 19 Public Awareness Campaign.I am eager to participate in putting the creative storytelling capabilities of young African through the lens. 
         `
 
       },
@@ -296,7 +295,7 @@ export class HomeComponent implements OnInit {
     dialogConfig.id = "modal-component";
     dialogConfig.height = "60%";
     dialogConfig.width = "60%";
-    dialogConfig.data = {'info': host.bio}
+    dialogConfig.data = { 'info': host.bio }
     // https://material.angular.io/components/dialog/overview
     const modalDialog = this.matDialog.open(HostProfileModalComponent, dialogConfig);
   }
@@ -308,7 +307,7 @@ export class HomeComponent implements OnInit {
     dialogConfig.id = "modal-component";
     dialogConfig.height = "60%";
     dialogConfig.width = "60%";
-    dialogConfig.data = {'info': info}
+    dialogConfig.data = { 'info': info }
     // https://material.angular.io/components/dialog/overview
     const modalDialog = this.matDialog.open(HostProfileModalComponent, dialogConfig);
   }
